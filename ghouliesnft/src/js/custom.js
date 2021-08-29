ddcd02ba41c558ecd243ad55945863fca46906e3
Matt Commons <matt@weaveweb.com.au>
2021-08-29 18:43:06 +1000

    theming

--- ghouliesnft/src/js/custom.js
+++ ghouliesnft/src/js/custom.js
@@ -183,5 +183,22 @@
 
 
 
+(function($) {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+}());
+
 
 })(jQuery);
